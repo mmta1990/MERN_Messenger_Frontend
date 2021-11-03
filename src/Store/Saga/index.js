06ae185cfc818af34675bd64211812239bc70613
mmta1990 <miladtaghipour1369@gmail.com>
2021-11-03 11:42:26 +0330

    Final Edition

---
+++ src/Store/Saga/index.js
@@ -0,0 +1,22 @@
+import { all } from "redux-saga/effects";
+
+
+
+import * as authHandler from "./auth";
+import * as mainHandler from "./main";
+import * as chatHandler from './chat';
+
+
+export default function* root() {
+    yield all([
+        authHandler.userRegisterWatcher(),
+        authHandler.userLoginWatcher(),
+        mainHandler.initWatcher(),
+        chatHandler.sendMessageWatcher(),
+        chatHandler.saveMessageWatcher(),
+        chatHandler.newChatWatcher(),
+        chatHandler.finishChatWatcher(),
+        mainHandler.updateOnlineUsersWatcher(),
+        chatHandler.activateRecentChatWatcher()
+    ]);
+}
